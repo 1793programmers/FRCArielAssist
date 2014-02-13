f14af28ad9b71c2d898dc04547fce7861411f721
GregKukanich <greg.kukanich@gmail.com>
2014-02-13 15:54:52 -0500

    Update RobotRunner.java

--- src/edu/wpi/first/wpilibj/templates/RobotRunner.java
+++ src/edu/wpi/first/wpilibj/templates/RobotRunner.java
@@ -6,7 +6,6 @@
 /*----------------------------------------------------------------------------*/
 package edu.wpi.first.wpilibj.templates;
 
-
 import edu.wpi.first.wpilibj.ADXL345_I2C;
 import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -16,7 +15,6 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -24,68 +22,72 @@ import edu.wpi.first.wpilibj.can.CANTimeoutException;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class RobotRunner extends IterativeRobot implements RobotComponent {
+public class RobotRunner extends IterativeRobot {
 
+    //Gunner is Awesome!
     //DRIVE FIELDS BELOW!
-    private final Joystick DRIVE_JOYSTICK = new Joystick(1);
+    private static final Joystick DRIVE_JOYSTICK = new Joystick(1);
     private CANJaguar fljag;  //Front Left Wheel Jag
     private CANJaguar rljag; //Rear Left Wheel Jag
     private CANJaguar frjag; //Front Right Wheel Jag
     private CANJaguar rrjag; //Rear Right Wheel Jag
-    private ADXL345_I2C accel = new ADXL345_I2C(1, ADXL345_I2C.DataFormat_Range.k2G);;
+    private ADXL345_I2C accel = new ADXL345_I2C(1, ADXL345_I2C.DataFormat_Range.k2G);
+    ;
     //ARM FIELDS BELOW!
-    private final Joystick ARM_JOYSTICK = new Joystick(2);
-    private final JoystickButton launchButt = new JoystickButton(ARM_JOYSTICK, 3);
-    private final JoystickButton retractButt = new JoystickButton(ARM_JOYSTICK, 2);
-    private final JoystickButton grabButt = new JoystickButton(ARM_JOYSTICK, 4); //4 on left side of joy, 5 on right side. in <-- out -->
-    private final JoystickButton releaseButt = new JoystickButton(ARM_JOYSTICK, 5);
-    private final Victor armVictor = new Victor(1); 
-    private final Victor grabVictor = new Victor(2);
-    private final Victor launchVic = new Victor(3);
+    private static final Joystick ARM_JOYSTICK = new Joystick(2);
+    private JoystickButton launchButt = new JoystickButton(ARM_JOYSTICK, 3);
+    private JoystickButton retractButt = new JoystickButton(ARM_JOYSTICK, 2);
+    private JoystickButton grabButt = new JoystickButton(ARM_JOYSTICK, 4); //4 on left side of joy, 5 on right side. in <-- out -->
+    private JoystickButton releaseButt = new JoystickButton(ARM_JOYSTICK, 5);
+    private Victor armVictor = new Victor(1);
+    private Victor grabVictor = new Victor(2);
+    private Victor launchVic = new Victor(3);
     //SWITCHES BELOW!
-    private final DigitalInput gFLimitSwitch = new DigitalInput(1);
-    private final DigitalInput gBLimitSwitch = new DigitalInput(2);
-    private final DigitalInput launchLimitSwitch = new DigitalInput(3);
+    private DigitalInput gFLimitSwitch = new DigitalInput(1);
+    private DigitalInput gBLimitSwitch = new DigitalInput(2);
+    private DigitalInput launchLimitSwitch = new DigitalInput(3);
     //TIME FOR BUSINESS! COMPONENTS INSTANTIATED BELOW!!
-    private DriveComponent driveComp = new DriveComponent(DRIVE_JOYSTICK, fljag, rljag, frjag, rrjag, accel);
-    private GrabComponent grabComp = new GrabComponent(ARM_JOYSTICK, grabButt, releaseButt, grabVictor);
-    private LaunchComponent launchComp = new LaunchComponent(ARM_JOYSTICK, launchButt, retractButt, launchVic);
-    private final LiftComponent liftComp = new LiftComponent(ARM_JOYSTICK, armVictor, gFLimitSwitch, gBLimitSwitch);
-    private CameraComponent cameraComp = new CameraComponent();
+    private DriveComponent driveComp;
+    private GrabComponent grabComp;
+    private LaunchComponent launchComp;
+    private LiftComponent liftComp;
+    private CameraComponent cameraComp;
     private RobotComponent[] components = {driveComp, grabComp, launchComp, liftComp, cameraComp};
     //Put all components above in an array to traverse later
-   
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void initialize() {
+    public void robotInit() {
         //Jag instantiations below
-        try {  
+        try {
             fljag = new CANJaguar(2); //Front Left Wheel Jag
             rljag = new CANJaguar(3); //Rear Left Wheel Jag
             frjag = new CANJaguar(4); //Front Right Wheel Jag
             rrjag = new CANJaguar(5); //Rear Right Wheel Jag
-            //drive = new RobotDrive(rljag, rrjag);
+            driveComp = new DriveComponent(DRIVE_JOYSTICK, fljag, rljag, frjag, rrjag, accel);
+
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
-        //Run each component's initialize method
-        for (int i = 0; i < components.length; i++) {
-            components[i].initialize();
-        }
+        grabComp = new GrabComponent(ARM_JOYSTICK, grabButt, releaseButt, grabVictor);
+        launchComp = new LaunchComponent(ARM_JOYSTICK, launchButt, retractButt, launchVic);
+        liftComp = new LiftComponent(ARM_JOYSTICK, armVictor, gFLimitSwitch, gBLimitSwitch);
+        cameraComp = new CameraComponent();
+
+//Run each component's initialize method
     }
 
     /**
      * This function is called periodically during autonomous
      */
-    
     public void autonomousInit() {
         for (int i = 0; i < components.length; i++) {
             components[i].autonomousInit();
         }
     }
-    
+
     public void autonomousPeriodic() {
         for (int i = 0; i < components.length; i++) {
             components[i].autonomousPeriodic();
@@ -95,25 +97,24 @@ public class RobotRunner extends IterativeRobot implements RobotComponent {
     /**
      * This function is called periodically during operator control
      */
-    
     public void disabledInit() {
         for (int i = 0; i < components.length; i++) {
             components[i].disabledInit();
         }
     }
-    
+
     public void disabledPeriodic() {
         for (int i = 0; i < components.length; i++) {
             components[i].disabledPeriodic();
         }
     }
-    
+
     public void teleopInit() {
         for (int i = 0; i < components.length; i++) {
             components[i].teleopInit();
         }
     }
-    
+
     public void teleopPeriodic() {
         for (int i = 0; i < components.length; i++) {
             components[i].teleopPeriodic();
