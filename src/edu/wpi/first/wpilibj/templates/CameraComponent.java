0987ca50e28a4dc55460b4878fc57ed009af1e8a
Kent Collins <kent.r.collins@gmail.com>
2014-02-16 16:22:10 -0800

    Update CameraComponent.java
    Following Sunday's Build

--- src/edu/wpi/first/wpilibj/templates/CameraComponent.java
+++ src/edu/wpi/first/wpilibj/templates/CameraComponent.java
@@ -4,68 +4,56 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStationLCD;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.camera.AxisCamera;
 import edu.wpi.first.wpilibj.camera.AxisCameraException;
 import edu.wpi.first.wpilibj.image.NIVisionException;
 import edu.wpi.first.wpilibj.image.ColorImage;
+
 /**
  *
  * @author milo
  */
 public class CameraComponent implements RobotComponent {
-        
+
     AxisCamera camera; //Camera
     private int imageWidth;//width of image
     //DriverStation dS = DriverStation.getInstance();
     ColorImage image;
-        
+
     public CameraComponent(AxisCamera c) {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-        
+
         camera = c;
         camera.writeMaxFPS(5);
         camera.writeResolution(AxisCamera.ResolutionT.k160x120);
         camera.writeCompression(20);
         camera.writeBrightness(0);
-        try {
-            camera.getImage();
-        } catch (AxisCameraException ex) {
-            ex.printStackTrace();
-        } catch (NIVisionException ex) {
-            ex.printStackTrace();
-        }
         System.out.println("Camera Activated");
     }
 
     public void autonomousPeriodic() {
         if (camera.getBrightness() == 100) {
             //shoot!
-         }
+        }
     }
 
     public void teleopPeriodic() {
-         //getting image from camera below
-         
-        //getWatchdog().setEnabled(false);
-         if (camera.freshImage()){
-         try {
-         image = camera.getImage();
-         image.free();
-         System.out.println(camera.getImage());
-         } catch (AxisCameraException ex) {
-         ex.printStackTrace();
-         } catch (NIVisionException ex) {
-         ex.printStackTrace();
-         }
-       }   
-         
-         camera.writeRotation(AxisCamera.RotationT.k180);
-         System.out.println(camera.getBrightness());
-         DriverStationLCD.getInstance().updateLCD();
+        if (camera.freshImage()) {
+            try {
+                image = camera.getImage();
+                image.free();
+            } catch (AxisCameraException ex) {
+                ex.printStackTrace();
+            } catch (NIVisionException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        //camera.writeRotation(AxisCamera.RotationT.k180);
+        System.out.println(camera.getBrightness());
+        DriverStationLCD.getInstance().updateLCD();
     }
 
     public void testPeriodic() {
@@ -75,16 +63,14 @@ public class CameraComponent implements RobotComponent {
     }
 
     public void autonomousInit() {
-        System.out.println("Camera Component initialized for autonomous"); 
-        
+        System.out.println("Camera Component initialized for autonomous");
+
     }
-    
+
     public void disabledInit() {
-    
     }
 
     public void teleopInit() {
-        System.out.println("Camera Component initialized for teleop"); 
+        System.out.println("Camera Component initialized for teleop");
     }
-
 }
