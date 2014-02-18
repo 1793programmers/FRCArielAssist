7e5b506edf5ac8490157973e4a6d57589f2a0700
Gunner1999 <dolphinleaf@gmail.com>
2014-02-18 14:27:16 -0500

    Update LaunchComponent.java

--- src/edu/wpi/first/wpilibj/templates/LaunchComponent.java
+++ src/edu/wpi/first/wpilibj/templates/LaunchComponent.java
@@ -4,229 +4,238 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.can.CANTimeoutException;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  * @author milo
  */
 public class LaunchComponent implements RobotComponent {
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-    private Victor launchVictor;
-    private Joystick launchStick;
-    private JoystickButton autoLaunchButton;
-    private JoystickButton manualLaunchButton;
-    private DigitalInput forwardLaunchSwitch; // channel 3 
-    private DigitalInput backwardLaunchSwitch; //channel 4
-    private Servo launchServo;
-    private boolean isLaunching = false;
+    // control inputs
+    private JoystickButton automaticButton;
+    private JoystickButton latchButton;
+    private JoystickButton cockButton;
+    private JoystickButton freezeButton;
+    private JoystickButton thawButton;
+    private JoystickButton launchButton;
+    // limit switches and sensor inputs
+    private DigitalInput latchLimitSwitch; // channel 3 
+    private DigitalInput cockLimitSwitch; //channel 4
+    // outputs and actuators
+    private Servo latchServo;
+    private Victor launcherVictor;
     private Timer timer;
-    private static final double timeout = 1.0;
+    // additional variables
+    private boolean isFrozen = false;
+    private boolean isLatched = false;
+    private boolean isCocked = false;
+    private boolean isAutomatic = false;
+    private static double moveToLatch = -1.0;
+    private static double moveToCock = 1.0;
+    private static int currentState;
+    // class constants
     private static final double UNLATCH_POSITION = 0.0;
-    private static final double LATCH_POSITION = 0.6425531914893617;
+    private static final double LATCH_POSITION = 0.6425531914893617; //0.225531914893617
+    private static final double TIMEOUT_DELAY = 1.0;
     public static final int NEUTRAL = 1;
-    public static final int MANUAL_LAUNCHING = 2;
-    public static final int AUTO_LAUNCHING = 3;
-    public static final int LAUNCH_FORWARD = 4;
-    public static final int LAUNCH_REVERSE = 5;
-    private static int currentState = NEUTRAL;
+    public static final int MANUAL_LATCHING = 2;
+    public static final int MANUAL_COCKING = 3;
+    public static final int AUTO_LATCHING = 4;
+    public static final int AUTO_COCKING = 5;
+    public static final int LAUNCHING = 6;
 
-    public LaunchComponent(Joystick j, JoystickButton jb1, JoystickButton jb2, Victor v, DigitalInput dI1, DigitalInput dI2, Servo s1) {
-        launchStick = j;
-        launchButton = jb1;
-        manualLaunchButton = jb2;
-        launchVictor = v;
-        forwardLaunchSwitch = dI1;
-        backwardLaunchSwitch = dI2;
-        launchServo = s1;
+    public LaunchComponent(JoystickButton auto,
+            JoystickButton latch,
+            JoystickButton cock,
+            JoystickButton stop,
+            JoystickButton thaw,
+            JoystickButton launch,
+            Victor victor,
+            DigitalInput fwdLimit,
+            DigitalInput backLimit,
+            Servo servo) {
+        automaticButton = auto;
+        latchButton = latch;
+        cockButton = cock;
+        freezeButton = stop;
+        thawButton = thaw;
+        launchButton = launch;
+        launcherVictor = victor;
+        latchLimitSwitch = fwdLimit;
+        cockLimitSwitch = backLimit;
+        latchServo = servo;
         timer = new Timer();
         currentState = NEUTRAL;
-    }
-
-    public void autonomousPeriodic() {
-    }
-
-    public void teleopInit() {
-        launchServo.set(UNLATCH_POSITION);
-        System.out.println("Launch Component initialized for teleop");
-        currentState = NEUTRAL;
-    }
-
-    public void teleopPeriodic() {
-
-        /*if (launchButton.get() == true){
-         lVictor.set(1);
-         Timer.delay(1);
-         // lVictor.set(-1);
-         }
-         else if (retractButton.get() == true){
-         lVictor.set(-1);
-         Timer.delay(1);
-         }
-         else {
-         lVictor.set(0);
-         }*/
-        /*
-         System.out.println(launchStick.getThrottle() + "     " + launchServo.get());
-         
-         System.out.println("Time =" + timer.get());
-         if (launchSwitch.get()) {
-         if (launchButton.get() == true) { //would like to launch
-         if (isLaunching == false) { // not currently in launch mode
-         timer.reset();
-         timer.start();
-         isLaunching = true;
-         launchServo.set(UNLATCH_POSITION);
-         lVictor.set(1.0);
-         } // if already in launch mode, no need to change anything
-         } else if (retractButton.get() == false) {
-         isLaunching = false;
-         launchServo.set(LATCH_POSITION);
-         } else { //don't want in launch
-         if (timer.get() > 1) { //if beyond time limit, stop launching
-         lVictor.set(-1.0);
-         launchServo.set(LATCH_POSITION);
-         isLaunching = false;
-         timer.reset();
-         timer.stop();
-         }// otherwise, we are not to the time limit, so don't change
-         }
-         } else {
-         lVictor.set(0);
-         }
-         */
-        //launchServo.set(launchStick.getThrottle());
-        if (RobotRunner.getArmFrozen()) {
-            launchVictor.set(0.0);
-        } else {
-            System.out.println(launchStick.getThrottle() + "     " + launchServo.get());
-            //launchServo.set(launchStick.getThrottle());
-            System.out.println("Time =" + timer.get());
-            double launchSignal = launchStick.getY();
-            
-            switch (currentState) {
-                
-                case NEUTRAL:
-                    launchVictor.set(0.0);
-                    launchServo.set(0.0);
-                    if (launchButton.get() == true) {//if in auto mode and button 2 pressed
-                        timer.reset();
-                        timer.start();
-                        currentState = LAUNCHING;
-                    }
-                    if (manualLaunchButton.get() {
-                        currentState = MANUAL_LATCHING;
-                    }
-
-                    break;
-                    
-                case MANUAL_LAUNCHING:
-                    launchVictor.set(1.0);
-                    //launchServo.set(0.0);
-                    if (autoLaunchButton.get() == true && RobotRunner.getManualMode() == false) {
-                        currentState = AUTO_LAUNCHING;
-                    }
-//                    if (manualLaunchButton.get() && RobotRunner.getManualMode() == true) {
-//                        MANUAL_LAUNCHING = MANUAL_LAUNCHING;
-//                    }
-                    if (launchSignal < 0 && RobotRunner.getManualMode() == true) {
-                        currentState = LAUNCH_REVERSE;
-                    }
-                    if (launchSignal > 0 && RobotRunner.getManualMode() == true) {
-                        currentState = LAUNCH_FORWARD;
-                    }
-
-                    break;
-                    
-                case AUTO_LAUNCHING:
-                    launchVictor.set(1.0);
-                    //launchServo.set(0.0);
-//                    if (autoLaunchButton.get() == true && RobotRunner.getManualMode() == false) {//if in auto mode and button 2 pressed
-//                        currentState = AUTO_LAUNCHING;
-//                    }
-                    if (manualLaunchButton.get() && RobotRunner.getManualMode() == true) {
-                        currentState = MANUAL_LAUNCHING;
-                    }
-                    if (launchSignal < 0 && RobotRunner.getManualMode() == true) {
-                        currentState = LAUNCH_REVERSE;
-                    }
-                    if (launchSignal > 0 && RobotRunner.getManualMode() == true) {
-                        currentState = LAUNCH_FORWARD;
-                    }
-
-                    break;
-                    
-                case LAUNCH_REVERSE:
-                    launchVictor.set(launchStick.getY());
-                    //launchServo.set(0.0);
-                    if (autoLaunchButton.get() == true && RobotRunner.getManualMode() == false) {//if in auto mode and button 2 pressed
-                        currentState = AUTO_LAUNCHING;
-                    }
-                    if (manualLaunchButton.get() && RobotRunner.getManualMode() == true) {
-                        currentState = MANUAL_LAUNCHING;
-                    }
-//                    if (launchSignal < 0 && RobotRunner.getManualMode() == true) {
-//                        currentState = LAUNCH_REVERSE;
-//                    }
-                    if (launchSignal > 0 && RobotRunner.getManualMode() == true) {
-                        currentState = LAUNCH_FORWARD;
-                    }
-
-                    break;
-                    
-                case LAUNCH_FORWARD:
-                    //launchServo.set(UNLATCH_POSITION);
-                    launchVictor.set(launchStick.getY());
-                    timer.reset();
-                    timer.start();
-                    //isLaunching = true;
-                    if (timer.get() > 1) {
-                        currentState = NEUTRAL;
-                    }
-                    launchVictor.set(launchStick.getY());
-                    launchServo.set(0.0);
-                    if (autoLaunchButton.get() == true && RobotRunner.getManualMode() == false) {//if in auto mode and button 2 pressed
-                        currentState = AUTO_LAUNCHING;
-                    }
-                    if (manualLaunchButton.get() && RobotRunner.getManualMode() == true) {
-                        currentState = MANUAL_LAUNCHING;
-                    }
-                    if (launchSignal < 0 && RobotRunner.getManualMode() == true) {
-                        currentState = LAUNCH_REVERSE;
-                    }
-//                    if (launchSignal > 0 && RobotRunner.getManualMode() == true) {
-//                        currentState = LAUNCH_FORWARD;
-//                    }
-
-                    break;
-            }
-        }
-    }
-
-    public void testPeriodic() {
-    }
-
-    public void disabledPeriodic() {
     }
 
     public void autonomousInit() {
         System.out.println("Launch Component initialized for autonomous");
     }
 
+    public void autonomousPeriodic() {
+        // TODO something useful
+    }
+
+    public void teleopInit() {
+        System.out.println("Launch Component initialized for teleop");
+        currentState = NEUTRAL;
+        isFrozen = false;
+    }
+
+    public void teleopPeriodic() {
+        printState();
+        if (freezeButton.get()) {
+            System.out.println("Freeze button pressed");
+            isFrozen = true;
+        } else if (thawButton.get()) {
+            System.out.println("Thaw button pressed");
+            isFrozen = false;
+        }
+        if (isFrozen) {
+            System.out.println("Launcher Frozen");
+            launcherVictor.set(0.0);
+        } else {
+            // collect sensor information, update latched/cocked
+            boolean isLatchLimitSwitchClosed = !latchLimitSwitch.get();
+            if (isLatchLimitSwitchClosed) {
+                System.out.println("Launcher now Latched");
+                isLatched = true;
+            }
+            boolean isCockLimitSwitchClosed = !cockLimitSwitch.get();
+            if (isCockLimitSwitchClosed) {
+                System.out.println("Launcher now Cocked");
+                isCocked = true;
+            } else {
+                isCocked = false;
+            }
+            //double latchSignal = launchStick.getY();
+            switch (currentState) {
+                case NEUTRAL:
+                    launcherVictor.set(0.0);
+                    latchServo.set(LATCH_POSITION);
+                    // allow to set automatic mode
+                    if (automaticButton.get()) {
+                        isAutomatic = true;
+                        currentState = AUTO_LATCHING;
+                        break;
+                    }
+                    if (launchButton.get()) {
+                        timer.reset();
+                        timer.start();
+                        currentState = LAUNCHING;
+                        break; // no need to continue
+                    }
+                    if (latchButton.get()) {
+                        isAutomatic = false;
+                        System.out.println("LatchButtonPressed");
+                        if (!isLatchLimitSwitchClosed) {
+                            System.out.println("Moving to Manual Latched State");
+                            currentState = MANUAL_LATCHING;
+                            break;
+                        }
+                        break;
+                    }
+                    if (cockButton.get()) {
+                        System.out.println("CockButtonPressed");
+                        isAutomatic = false;
+                        if (!isCockLimitSwitchClosed) {
+                            System.out.println("Moving to Manual Cock State");
+                            currentState = MANUAL_COCKING;
+                            break;
+                        }
+                        break;
+                    }
+                    if (isAutomatic) {
+                        if (!isLatched) {
+                            currentState = AUTO_LATCHING;
+                        } else if (!isCocked) {
+                            currentState = AUTO_COCKING;
+                        }
+                    }
+                case MANUAL_LATCHING:
+                    launcherVictor.set(moveToLatch);
+                    latchServo.set(LATCH_POSITION);
+                    isAutomatic = false;
+                    if (isLatchLimitSwitchClosed || !latchButton.get()) {
+                        launcherVictor.set(0.0);
+                        currentState = NEUTRAL;
+                    }
+                    break;
+
+                case MANUAL_COCKING:
+                    launcherVictor.set(moveToCock);
+                    latchServo.set(LATCH_POSITION);
+                    isAutomatic = false;
+                    if (isCockLimitSwitchClosed || !cockButton.get()) {
+                        launcherVictor.set(0.0);
+                        currentState = NEUTRAL;
+                    }
+                    break;
+
+                case AUTO_LATCHING:
+                    launcherVictor.set(moveToLatch);
+                    latchServo.set(LATCH_POSITION);
+                    if (latchButton.get() || cockButton.get()) {  // go manual
+                        launcherVictor.set(0.0);
+                        currentState = NEUTRAL;
+                        break;
+                    }
+                    if (isLatched) { // latching complete
+                        launcherVictor.set(0.0);
+                        currentState = AUTO_COCKING;
+                        break;
+                    }
+                    break;
+
+                case AUTO_COCKING:
+                    launcherVictor.set(moveToCock);
+                    latchServo.set(LATCH_POSITION);
+                    if (latchButton.get() || cockButton.get()) { // go manual
+                        launcherVictor.set(0.0);
+                        currentState = NEUTRAL;
+                        break;
+                    }
+                    if (isCocked) { // cocking complete
+                        launcherVictor.set(0.0);
+                        currentState = NEUTRAL;
+                        break;
+                    }
+                    break;
+
+                case LAUNCHING:
+                    latchServo.set(UNLATCH_POSITION);
+                    launcherVictor.set(0.0);
+                    isLatched = false;
+                    isCocked = false;
+                    if (timer.get() > TIMEOUT_DELAY) {
+                        if (isAutomatic) {
+                            currentState = AUTO_LATCHING;
+                        } else {
+                            currentState = NEUTRAL;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    public void disabledPeriodic() {
+    }
+
     public void disabledInit() {
     }
 
-    public boolean isLaunching() {
-        return isLaunching;
+    public int getState() {
+        return currentState;
+    }
+    
+    private void printState(){
+        System.out.println("Launcher state: " + currentState + " Latched: " + isLatched + " Cocked: " + isCocked + " Frozen: " + isFrozen + " Automatic: " + isAutomatic);
+        
     }
 }
