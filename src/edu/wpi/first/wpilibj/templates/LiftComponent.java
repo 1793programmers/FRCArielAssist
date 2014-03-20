494e90098661c5729daaef0a88b3c648eb0717ef
Leonard Ramsey <lar.leo96@gmail.com>
2014-03-19 21:34:41 -0400

    Update LiftComponent.java

--- src/edu/wpi/first/wpilibj/templates/LiftComponent.java
+++ src/edu/wpi/first/wpilibj/templates/LiftComponent.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  *
@@ -16,68 +15,106 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  */
 public class LiftComponent implements RobotComponent {
 
+    /**
+     * @return the currentState
+     */
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     private Joystick armStick;
-    private JoystickButton resetButton;
+    //private JoystickButton resetButton;
     private Victor armVictor;
     static DigitalInput fLimitSwitch;
     static DigitalInput bLimitSwitch;
-    private AnalogChannel ultrasonic = RobotRunner.getUltrasonicSensor();
+    private AnalogChannel ultrasonic;
     public static final int NEUTRAL = 1;
     public static final int DEPLOYING = 2;
     public static final int RETRACTING = 3;
     public static final int RESETTING = 4;
+    //AUTONOMOUS STATES
+    public static final int READY = 5;
+    public static final int LAUNCH = 6;
     private static int currentState = NEUTRAL;
     static boolean isBLimitOpen;
-        //for now, if ultrasonic sensor returns 7 feet in autonomous, launch with a timer delay to stop (can be anywhere btwn 0 & 2 seconds)
-    
-    public LiftComponent(Joystick j, Victor v, DigitalInput s1, DigitalInput s2, JoystickButton b) {
+
+    public LiftComponent(Joystick j, Victor v, DigitalInput s1, DigitalInput s2) {
         armStick = j;
-        resetButton = b;
+        //resetButton = b;
         armVictor = v;
         fLimitSwitch = s1;
         bLimitSwitch = s2;
         currentState = NEUTRAL;
+        isBLimitOpen = bLimitSwitch.get();
+        ultrasonic = RobotRunner.getUltrasonicSensor();
     }
 
     public void autonomousInit() {
         armVictor.set(-1.0); //move to shooting position
-        isBLimitOpen = bLimitSwitch.get();
+        //  boolean isBLimitOpen = bLimitSwitch.get();
         if (!isBLimitOpen) { //when limitswitch is closed the arm will stop
             armVictor.set(0);
         }
         System.out.println("Lift Component initialized for autonomous");
+        currentState = DEPLOYING;
     }
 
     public void autonomousPeriodic() {
+        boolean isForwardLimitSwitchShut = !fLimitSwitch.get();
+        boolean isBackwardLimitSwitchShut = !bLimitSwitch.get();
+        printState(isForwardLimitSwitchShut, isBackwardLimitSwitchShut);
+        armVictor.set(0.0);
+        double armSignal = 1.0;
+        switch (getCurrentState()) {
+            case DEPLOYING:
+                if (isBackwardLimitSwitchShut) {
+                    armVictor.set(armSignal);
+                }
+                if (!isBackwardLimitSwitchShut) {
+                    currentState = RETRACTING;
+                }
+                break;
+            case RETRACTING:
+                if (!isBackwardLimitSwitchShut) {
+                    armVictor.set(-armSignal);
+                }
+                if (isBackwardLimitSwitchShut) {
+                    armVictor.set(0);
+                    currentState = READY;
+                }
+                break;
+            case READY:
+                if (DriveComponent.getCurrentState() == DriveComponent.LAUNCH) {
+                    currentState = LAUNCH;
+                }
+                break;
+
+            case LAUNCH:
+                //Nothing, placeholder to check at this time
+                break;
+        }
     }
 
     public void teleopInit() {
-        System.out.println("Lift Component initialized for teleop");
+        //System.out.println("Lift Component initialized for teleop");
         currentState = NEUTRAL;
     }
 
     public void teleopPeriodic() {
         boolean isForwardLimitSwitchShut = !fLimitSwitch.get();
         boolean isBackwardLimitSwitchShut = !bLimitSwitch.get();
-//        printState(isForwardLimitSwitchShut, isBackwardLimitSwitchShut);
+       // printState(isForwardLimitSwitchShut, isBackwardLimitSwitchShut);
         armVictor.set(0.0);
         double armSignal = armStick.getThrottle();
-        currentState=NEUTRAL;
-        
-        if (armSignal < -0.1) { 
-//            System.out.println("Moving Arm Backwards");
+
+        if (armSignal < -0.1) { // Retracting Backward
+            //System.out.println("Moving Arm Backwards");
             if (!isBackwardLimitSwitchShut) {
                 armVictor.set(-armSignal);
-                currentState=RETRACTING;
             }
 
-        } else if (armSignal >0.1) { 
-//            System.out.println("Moving Arm Forward");
+        } else if (armSignal > 0.1) { // Retracting backward
+            //System.out.println("Moving Arm Forward");
             if (!isForwardLimitSwitchShut) {
                 armVictor.set(-armSignal);
-                currentState=DEPLOYING;
             }
         }
     }
@@ -87,8 +124,12 @@ public class LiftComponent implements RobotComponent {
 
     public void disabledPeriodic() {
     }
-    
-    public int getCurrentState(){
+
+    private void printState(boolean a, boolean b) {
+       // System.out.println("Forward Limit =" + a + "Back Limit =" + b);
+    }
+
+    public static int getCurrentState() {
         return currentState;
     }
 }
